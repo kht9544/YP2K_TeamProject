e940d5cd6661da987c58d3dd2579ffff91bb4261
kht9544 <137915554+kht9544@users.noreply.github.com>
2024-09-21 18:47:56 +0900

    ShakeCamera
    ShakeCamera

---
+++ Prototype/Source/Prototype/Monster/NormalMonster.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NormalMonster.generated.h"
+
+UCLASS()
+class PROTOTYPE_API ANormalMonster : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ANormalMonster();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	float GetLaunchLength(){return _launchLength;}
+
+	UFUNCTION()
+    void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+    void LaunchFromPlayer(FVector LaunchDirection, float LaunchStrength);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Physics", meta = (AllowPrivateAccess = "true"))
+    float _launchLength;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+    class UCapsuleComponent* _capsuleComponent;
+
+
+};
