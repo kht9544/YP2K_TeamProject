1f88076f053b703995657eb99aec0be5ba6e0802
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-11-21 18:16:02 +0900

    YSR_Build : TriggerBox 내용 지움

--- Prototype/Source/Prototype/TriggerBox_StageSequnce/StageSequence_Trigger.h
+++ Prototype/Source/Prototype/TriggerBox_StageSequnce/StageSequence_Trigger.h
@@ -37,38 +37,4 @@ public:
 
 
 
-
-private:
-
-	UPROPERTY(EditAnywhere, Category = "TriggerBox")
-	UBoxComponent* _boxComponent;
-
-	UPROPERTY(EditAnywhere, Category = "UI")
-	TArray<UUserWidget*> UIWidgets;
-
-	UPROPERTY(EditAnywhere, Category = "Sequence")
-	FString SequencePath;
-
-	UPROPERTY()
-	ULevelSequencePlayer* _sequencePlayer;
-	
-
-	UFUNCTION()
-	void OnOverlapBegin(
-		UPrimitiveComponent* OverlappedComponent,
-		AActor* OtherActor,
-		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyIndex,
-		bool bFromSweep,
-		const FHitResult& SweepResult
-	);
-
-	UFUNCTION()
-	void OnSequenceFinished();
-
-	void HideUIWidgets();
-	void ShowUIWidgets();
-	void DestroyTriggerBox();
-
-
 };
